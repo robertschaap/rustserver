9406d5c8adc94d6a94a6fde1db56e87b8858159c
robertschaap <robert.schaap@mac.com>
2018-08-19 19:28:43 +0200

    main.rs: added gcd function

--- src/main.rs
+++ src/main.rs
@@ -31,3 +31,17 @@ fn get_form(_request: &mut Request) -> IronResult<Response> {
 
     Ok(response)
 }
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
